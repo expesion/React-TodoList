af9b199be42d5030e77053ec7b7f7d52c6c3e95d
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-01-31 16:36:40 +0530

    Editing todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
 
-function Todo({task,removeTodo,id}) {
-    return (
-        <div>
-            <button>Edit</button>
-            <button onClick={()=>removeTodo(id)}>X</button>
-            <li>{task}</li>
-        </div>
-    )
+function Todo({ task, removeTodo, id, update }) {
+  let [editing, setEditing] = useState(false);
+  let updateTodo = useRef();
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    update(id, updateTodo.current.value);
+    setEditing(false);
+  };
+  return editing ? (
+    <div>
+      <form onSubmit={handleUpdate}>
+        <input type="text" ref={updateTodo} defaultValue={task} />
+        <button>Save</button>
+      </form>
+    </div>
+  ) : (
+    <div>
+      <button onClick={() => setEditing(!editing)}>Edit</button>
+      <button onClick={() => removeTodo(id)}>X</button>
+      <li>{task}</li>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,12 +6,27 @@ function TodoList() {
   const removeTodo = (id) => {
     setTodos((todo) => todo.filter((t) => t.id !== id));
   };
+  const updateTodo = (id, newTask) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, task: newTask };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <div>
       <h1>Todo List!</h1>
       <ul>
         {todos.map(({ task, id }) => (
-          <Todo key={id} id={id} task={task} removeTodo={removeTodo} />
+          <Todo
+            key={id}
+            id={id}
+            task={task}
+            removeTodo={removeTodo}
+            update={updateTodo}
+          />
         ))}
       </ul>
       <CreateTodo addTodo={setTodos} />
